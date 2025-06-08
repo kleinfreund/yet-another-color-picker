c98d5dfdfccb1c38acf2af8706087737088b90ff
kleinfreund <5774638+kleinfreund@users.noreply.github.com>
2025-06-08 10:48:49 +0200

    chore: replace tsup-based build setup with vite-plugin-dts

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,27 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vitest/config'
+import dts from 'vite-plugin-dts'
+
+import pkg from './package.json' with { type: 'json' }
 
 export default defineConfig({
+	plugins: [
+		dts({ rollupTypes: true }),
+	],
+
+	build: {
+		lib: {
+			entry: fileURLToPath(new URL('./src/ColorPicker.ts', import.meta.url)),
+			fileName: 'ColorPicker',
+			formats: ['es'],
+		},
+		rollupOptions: {
+			// Prevents bundling peer dependencies.
+			external: Object.keys(pkg.peerDependencies),
+		},
+	},
+
 	test: {
 		environment: 'jsdom',
 		coverage: {
